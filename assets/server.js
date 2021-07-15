34f102e890112211026118c06b46de544134d63f
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-07-14 23:14:41 -0700

    more updates to portfolio

---
+++ assets/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
